05391f0a0122c61773c4545323d9e656b3b481cc
Valentin CHAILLOU <vchaillo@e3r5p10.42.fr>
2014-12-15 20:54:34 +0100

    modif compil.sh

--- projet/compil.sh
+++ projet/compil.sh
@@ -1,3 +1,4 @@
 gcc -Wall -Wextra -Werror -I libft/includes/ -c main.c mlx.c parser.c
 gcc -o mlx_prog main.o mlx.o parser.o -Llibft -lft  -L/usr/X11/lib -lmlx -lXext -lX11
+rm *.o
 echo "\033[32mmlx_prog was created\033[0m"
