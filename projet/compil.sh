bd5535e57eb926a343c877ea822fa66e722f9a8b
Valentin CHAILLOU <vchaillo@e3r6p9.42.fr>
2015-01-27 17:20:42 +0100

    modif de compil.sh

--- projet/compil.sh
+++ projet/compil.sh
@@ -1,3 +1,5 @@
+make -C libft/ fclean
+make -C libft/
 gcc -Wall -Wextra -Werror -I libft/includes/ -c main.c mlx.c parser.c draw.c error_msg.c utils.c
 gcc -o mlx_prog main.o mlx.o parser.o draw.o error_msg.o utils.o -Llibft -lft  -L/usr/X11/lib -lmlx -lXext -lX11
 rm *.o
