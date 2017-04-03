832e9c3700226c4f2e8437a1723916fe9a229e96
vchaillo <valentinchaillou89@gmail.com>
2017-04-03 08:55:39 +0200

    add keycodes.h

---
+++ inc/keycodes.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   keycodes.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vchaillo <vchaillo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/10/04 12:22:32 by vchaillo          #+#    #+#             */
+/*   Updated: 2017/04/03 08:36:03 by valentin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef KEYCODES_H
+# define KEYCODES_H
+
+/*
+** linux azerty keycodes
+*/
+# define KEY_R 114
+# define KEY_ESCAPE 65307
+
+/*
+** mac osx qwerty keycodes
+*/
+# define KEY_R_MAC 15
+# define KEY_ESCAPE_MAC 53
+
+#endif
